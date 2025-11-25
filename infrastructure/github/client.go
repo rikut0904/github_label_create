37379bf7666f81d8cb8f09a37ca7f680b0e9b755
rikut0904 <rkt9tuba4robot@gmail.com>
2025-11-25 15:21:37 +0900

    del/label_create

--- infrastructure/github/client.go
+++ infrastructure/github/client.go
@@ -37,68 +37,6 @@ func (c *GitHubClient) getClient(installationID int64) (*github.Client, error) {
 	return github.NewClient(&http.Client{Transport: itr}), nil
 }
 
-func (c *GitHubClient) ListLabels(ctx context.Context, repo entity.Repository) ([]entity.Label, error) {
-	client, err := c.getClient(repo.InstallationID)
-	if err != nil {
-		return nil, err
-	}
-
-	opts := &github.ListOptions{PerPage: 100}
-	var labels []entity.Label
-
-	for {
-		ghLabels, resp, err := client.Issues.ListLabels(ctx, repo.Owner, repo.Name, opts)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, l := range ghLabels {
-			labels = append(labels, entity.Label{
-				Name:        l.GetName(),
-				Color:       l.GetColor(),
-				Description: l.GetDescription(),
-			})
-		}
-
-		if resp == nil || resp.NextPage == 0 {
-			break
-		}
-		opts.Page = resp.NextPage
-	}
-
-	return labels, nil
-}
-
-func (c *GitHubClient) DeleteLabel(ctx context.Context, repo entity.Repository, name string) error {
-	client, err := c.getClient(repo.InstallationID)
-	if err != nil {
-		return err
-	}
-
-	_, err = client.Issues.DeleteLabel(ctx, repo.Owner, repo.Name, name)
-	if err != nil {
-		if respErr, ok := err.(*github.ErrorResponse); ok && respErr.Response != nil && respErr.Response.StatusCode == http.StatusNotFound {
-			return nil
-		}
-	}
-	return err
-}
-
-func (c *GitHubClient) CreateLabel(ctx context.Context, repo entity.Repository, label entity.Label) error {
-	client, err := c.getClient(repo.InstallationID)
-	if err != nil {
-		return err
-	}
-
-	_, _, err = client.Issues.CreateLabel(ctx, repo.Owner, repo.Name, &github.Label{
-		Name:        github.String(label.Name),
-		Color:       github.String(label.Color),
-		Description: github.String(label.Description),
-	})
-
-	return err
-}
-
 func (c *GitHubClient) CreateFile(ctx context.Context, repo entity.Repository, workflow entity.Workflow) error {
 	client, err := c.getClient(repo.InstallationID)
 	if err != nil {
--- usecase/setup_repository.go
+++ usecase/setup_repository.go
@@ -21,51 +21,12 @@ func NewSetupRepositoryUseCase(githubRepo repository.GitHubRepository) *SetupRep
 func (uc *SetupRepositoryUseCase) Execute(ctx context.Context, repo entity.Repository) error {
 	log.Printf("Setting up repository: %s/%s", repo.Owner, repo.Name)
 
-	// 既存のラベルを削除
-	if err := uc.deleteExistingLabels(ctx, repo); err != nil {
-		log.Printf("Error deleting existing labels: %v", err)
-	}
-
-	// 新しいラベルを作成
-	if err := uc.createLabels(ctx, repo); err != nil {
-		log.Printf("Error creating labels: %v", err)
-	}
-
 	// ワークフローファイルを作成
 	if err := uc.createWorkflow(ctx, repo); err != nil {
 		log.Printf("Error creating workflow: %v", err)
 	}
 
 	log.Printf("Repository setup completed: %s/%s", repo.Owner, repo.Name)
-	return nil
-}
-
-func (uc *SetupRepositoryUseCase) deleteExistingLabels(ctx context.Context, repo entity.Repository) error {
-	labels, err := uc.githubRepo.ListLabels(ctx, repo)
-	if err != nil {
-		return err
-	}
-
-	for _, label := range labels {
-		if err := uc.githubRepo.DeleteLabel(ctx, repo, label.Name); err != nil {
-			log.Printf("Error deleting label %s: %v", label.Name, err)
-		}
-	}
-
-	return nil
-}
-
-func (uc *SetupRepositoryUseCase) createLabels(ctx context.Context, repo entity.Repository) error {
-	labels := entity.DefaultLabels()
-
-	for _, label := range labels {
-		if err := uc.githubRepo.CreateLabel(ctx, repo, label); err != nil {
-			log.Printf("Error creating label %s: %v", label.Name, err)
-		} else {
-			log.Printf("Created label: %s", label.Name)
-		}
-	}
-
 	return nil
 }
 
